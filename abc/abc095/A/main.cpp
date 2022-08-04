c4e447c549c9f3f9e955e9299bfe3c2efcec21bc
bwsunkist <fdamasahiro@DESKTOP-3SGN2SD.localdomain>
2022-08-04 22:17:41 +0900

    abc:095

--- abc/abc095/A/main.cpp
+++ abc/abc095/A/main.cpp
@@ -11,44 +11,13 @@
 using namespace std;
 
 int main() {
-  int N, M;
-  int ans = 0;
   string s;
+  int price = 700;
 
-  cin >> N;
+  cin >> s;
 
-  vector<int> Am(A);
-  vector<pair<int,int>> AB(N);
-  for(int i = 0; i < N; i++){
-    AB[i] = make_pair(A[i], B[i]);
+  for(int i = 0; i < 3; i++) {
+    if(s[i] == 'o') price += 100;
   }
-   sort(AB.begin(), AB.end());
-
-   vector<vector<int>> s(M, vector<int>(N));
-  
-  for(int i = 0; i < N; i++){
-  }
-
-  for(int i = 0; i < s.length(); i++){
-  }
-
-  //compare string /char
-  s[loopcnt] == 'A';
-
-  cout << ans << endl;
-
-  //小数点以下指定
-  cout << fixed;
-  cout << setprecision(5) << ans << endl;
-
-  string s1;
-  istringstream ss1;
-  int p1;
-
-  //string >> int
-  cin >> s1;
-  ss1 = istringstream(s1);
-  ss1 >> p1;
-
-
+  cout << price << endl;
 }
--- abc/abc095/B/main.cpp
+++ abc/abc095/B/main.cpp
@@ -11,44 +11,22 @@
 using namespace std;
 
 int main() {
-  int N, M;
-  int ans = 0;
+  int N, X;
   string s;
+  int used = 0;
 
-  cin >> N;
+  cin >> N >> X;
 
-  vector<int> Am(A);
-  vector<pair<int,int>> AB(N);
+  vector<int> m(N);
   for(int i = 0; i < N; i++){
-    AB[i] = make_pair(A[i], B[i]);
-  }
-   sort(AB.begin(), AB.end());
-
-   vector<vector<int>> s(M, vector<int>(N));
-  
-  for(int i = 0; i < N; i++){
+    cin >> m[i];
+    used += m[i];
   }
 
-  for(int i = 0; i < s.length(); i++){
-  }
+  sort(m.begin(), m.end());
+  int min = m[0];
 
-  //compare string /char
-  s[loopcnt] == 'A';
-
-  cout << ans << endl;
-
-  //小数点以下指定
-  cout << fixed;
-  cout << setprecision(5) << ans << endl;
-
-  string s1;
-  istringstream ss1;
-  int p1;
-
-  //string >> int
-  cin >> s1;
-  ss1 = istringstream(s1);
-  ss1 >> p1;
-
-
+  int remind = X - used;
+  int re = remind / min;
+  cout << N + re << endl;
 }
